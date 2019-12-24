6df4f18ec3f588e60a77574bfb0931c953d8e5eb
EugenePetrik <eugene.petrik@rubygarage.org>
2019-12-24 14:25:58 +0200

    Add specs for File Upload page

---
+++ spec/feature/file_upload_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+RSpec.feature 'File Upload page' do
+  before { visit '/upload' }
+
+  it 'checks page title' do
+    expect(page).to have_css('h3', text: 'File Uploader')
+  end
+
+  it 'uploads file' do
+    path = File.absolute_path('./spec/support/files/dummy_image.jpeg')
+
+    attach_file('file-upload', path)
+    click_on 'Upload'
+
+    expect(page).to have_css('h3', text: 'File Uploaded!')
+    expect(page).to have_css('#uploaded-files', text: "#{File.basename(path)}")
+  end
+end
